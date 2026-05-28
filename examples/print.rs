232d1c5931201722838250501271a1d2852363ef
Dylan Gardham <message4dylan@gmail.com>
2026-05-28 20:51:31 +0100

    char rectangle (print backend specific widget)

--- examples/print.rs
+++ examples/print.rs
@@ -1,5 +1,5 @@
 use angui::{
-    backends, containers::horizontal_container::HorizontalContainer, layout_traits::Render,
+    backends::{self, print_backend::CharRectangle}, containers::horizontal_container::HorizontalContainer, layout_traits::Render,
     position::Position, widgets::rectangle::RectangleElement,
 };
 
@@ -7,8 +7,8 @@ fn main() {
     let mut ctx = backends::print_backend::PrintBackendCTX::new(150, 50);
 
     let root = HorizontalContainer::new()
-        .add_child(RectangleElement::new(30, 10))
-        .add_child(RectangleElement::new(5, 15));
+        .add_child(CharRectangle::new(30, 10, 'a'))
+        .add_child(CharRectangle::new(5, 15, 'b'));
 
     root.render(&mut ctx, Position::new(0, 0)); // render onto buffer
     ctx.display(); // print the buffer to the terminal
--- src/backends/print_backend.rs
+++ src/backends/print_backend.rs
@@ -1,4 +1,7 @@
-use crate::{layout_traits::Render, widgets::rectangle::RectangleElement};
+use crate::{
+    layout_traits::{KnownXSizeElement, KnownYSizeElement, Render},
+    widgets::rectangle::RectangleElement,
+};
 
 pub struct PrintBackendCTX {
     pub width: usize,
@@ -17,7 +20,11 @@ impl PrintBackendCTX {
 
     pub fn display(self) {
         for line in self.buffer {
-            println!("{}", line.iter().fold("".to_string(), |l, r| format!("{}{}", l, r).to_string()))
+            println!(
+                "{}",
+                line.iter()
+                    .fold("".to_string(), |l, r| format!("{}{}", l, r).to_string())
+            )
         }
     }
 }
@@ -29,5 +36,43 @@ impl Render<PrintBackendCTX> for RectangleElement {
                 ctx.buffer[top_left.y + row][top_left.x + column] = '█';
             }
         }
+    }
+}
+
+pub struct CharRectangle {
+    width: usize,
+    height: usize,
+    char: char,
+}
+
+impl CharRectangle {
+    pub fn new(width: usize, height: usize, char: char) -> Self {
+        Self {
+            width,
+            height,
+            char,
+        }
+    }
+}
+
+impl Render<PrintBackendCTX> for CharRectangle {
+    fn render(&self, ctx: &mut PrintBackendCTX, top_left: crate::position::Position) {
+        for y in 0..self.height {
+            for x in 0..self.width {
+                ctx.buffer[top_left.y + y][top_left.x + x] = self.char;
+            }
+        }
+    }
+}
+
+impl KnownXSizeElement for CharRectangle {
+    fn get_x_size(&self) -> usize {
+        self.width
+    }
+}
+
+impl KnownYSizeElement for CharRectangle {
+    fn get_y_size(&self) -> usize {
+        self.height
     }
 }
