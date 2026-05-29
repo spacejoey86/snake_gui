d94f7dc48b13cccf07c6d688346ea2f7a474ed7b
Dylan Gardham <message4dylan@gmail.com>
2026-05-29 15:40:00 +0100

    example fits to size

--- examples/print.rs
+++ examples/print.rs
@@ -3,7 +3,10 @@ use angui::{
         self,
         print_backend::{CharRectangle, PrintBackendCTX},
     },
-    containers::{border_container::BorderContainer, horizontal_container::HorizontalContainer, padding_container::PaddingContainer},
+    containers::{
+        border_container::BorderContainer, horizontal_container::HorizontalContainer,
+        padding_container::PaddingContainer,
+    },
     position::Position,
     traits::{FixedHeight, FixedWidth, Render},
     widgets::{label::Label, rectangle::RectangleElement},
@@ -13,13 +16,15 @@ trait SizedPrint: Render<PrintBackendCTX> + FixedWidth + FixedHeight {}
 impl<T> SizedPrint for T where T: Render<PrintBackendCTX> + FixedWidth + FixedHeight {}
 
 fn main() {
-    let mut ctx = backends::print_backend::PrintBackendCTX::new(150, 50);
+    let root = BorderContainer::new(Box::new(
+        HorizontalContainer::new()
+            .add_child(CharRectangle::new(30, 10, 'a') as Box<dyn SizedPrint>)
+            .add_child(PaddingContainer::all(Label::new("Test label"), 1))
+            .add_child(CharRectangle::new(5, 15, 'b'))
+            .add_child(RectangleElement::new(2, 2)),
+    ));
 
-    let root = BorderContainer::new(Box::new(HorizontalContainer::new()
-        .add_child(CharRectangle::new(30, 10, 'a') as Box<dyn SizedPrint>)
-        .add_child(PaddingContainer::all(Label::new("Test label"), 1))
-        .add_child(CharRectangle::new(5, 15, 'b'))
-        .add_child(RectangleElement::new(2, 2))));
+    let mut ctx = backends::print_backend::PrintBackendCTX::new(root.width(), root.height());
 
     root.render(&mut ctx, Position::new(0, 0)); // render onto buffer
     ctx.display(); // print the buffer to the terminal
