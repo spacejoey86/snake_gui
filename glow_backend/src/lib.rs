c01b5898bc67b1f5bbf11648de771a0f58916966
Dylan Gardham <message4dylan@gmail.com>
2026-05-31 12:35:06 +0100

    add sizes to rectangles

--- glow_backend/src/lib.rs
+++ glow_backend/src/lib.rs
@@ -5,26 +5,28 @@ use glow::{Context, HasContext, NativeBuffer};
 pub struct GlowBackendContext {
     gl: Context,
     instance_offset_buffer: NativeBuffer,
+    instance_size_buffer: NativeBuffer,
     rects: Vec<Rect>,
 }
 
 struct Rect {
     offset_x: f32,
     offset_y: f32,
-    // width: f32,
-    // height: f32,
+    width: f32,
+    height: f32,
 }
 
 const VERTEX_SHADER_SOURCE: &str = "
 #version 330 core
 layout (location = 0) in vec2 vertexPos;
 layout (location = 1) in vec2 instanceOffset;
+layout (location = 2) in vec2 instanceSize;
 
 out vec3 fColor;
 
 void main()
 {
-    gl_Position = vec4(vertexPos + instanceOffset, 0.0, 1.0);
+    gl_Position = vec4(vertexPos * instanceSize + instanceOffset, 0.0, 1.0);
     fColor = vec3(1.0, 0.0, 1.0);
 }
 ";
@@ -89,25 +91,38 @@ impl GlowBackendContext {
             gl.enable_vertex_attrib_array(1);
             gl.vertex_attrib_pointer_f32(1, 2, glow::FLOAT, false, 0, 0); // should the stride be set?
             gl.vertex_attrib_divisor(1, 1);
+            // instance size buffer
+            let instance_size_buffer = gl.create_buffer().unwrap();
+            gl.bind_buffer(glow::ARRAY_BUFFER, Some(instance_size_buffer));
+            gl.enable_vertex_attrib_array(2);
+            gl.vertex_attrib_pointer_f32(2, 2, glow::FLOAT, false, 0, 0);
+            gl.vertex_attrib_divisor(2, 1);
 
             // todo: font texture
 
             Self {
                 gl,
                 instance_offset_buffer,
+                instance_size_buffer,
                 rects: vec![
                     Rect {
                         offset_x: 0.0,
                         offset_y: 0.0,
+                        width: 1.0,
+                        height: 1.0,
                     },
                     Rect {
                         offset_x: -0.5,
                         offset_y: -0.5,
+                        width: 0.1,
+                        height: 1.0,
                     },
                     Rect {
                         offset_x: -1.2,
                         offset_y: 0.0,
-                    }
+                        width: 1.0,
+                        height: 1.0,
+                    },
                 ],
             }
         }
@@ -116,16 +131,41 @@ impl GlowBackendContext {
     pub fn display(&self) {
         unsafe {
             // upload instance attributes
-            self.gl.bind_buffer(glow::ARRAY_BUFFER, Some(self.instance_offset_buffer));
-            let instance_offsets: Vec<f32> = self.rects.iter().flat_map(|rect| [rect.offset_x, rect.offset_y]).collect();
+            // offsets
+            self.gl
+                .bind_buffer(glow::ARRAY_BUFFER, Some(self.instance_offset_buffer));
+            let instance_offsets: Vec<f32> = self
+                .rects
+                .iter()
+                .flat_map(|rect| [rect.offset_x, rect.offset_y])
+                .collect();
             let instance_offsets_u8 = core::slice::from_raw_parts(
                 instance_offsets.as_ptr() as *const u8,
                 instance_offsets.len() * core::mem::size_of::<f32>(),
             );
-            self.gl.buffer_data_u8_slice(glow::ARRAY_BUFFER, instance_offsets_u8, glow::STATIC_DRAW);
+            self.gl.buffer_data_u8_slice(
+                glow::ARRAY_BUFFER,
+                instance_offsets_u8,
+                glow::STATIC_DRAW,
+            );
+            // sizes
+            self.gl
+                .bind_buffer(glow::ARRAY_BUFFER, Some(self.instance_size_buffer));
+            let instance_sizes: Vec<f32> = self
+                .rects
+                .iter()
+                .flat_map(|rect| [rect.width, rect.height])
+                .collect();
+            let instance_sizes_u8 = core::slice::from_raw_parts(
+                instance_sizes.as_ptr() as *const u8,
+                instance_sizes.len() * core::mem::size_of::<f32>(),
+            );
+            self.gl
+                .buffer_data_u8_slice(glow::ARRAY_BUFFER, instance_sizes_u8, glow::STATIC_DRAW);
 
             // draw
-            self.gl.draw_arrays_instanced(glow::TRIANGLE_STRIP, 0, 4, self.rects.len() as i32);
+            self.gl
+                .draw_arrays_instanced(glow::TRIANGLE_STRIP, 0, 4, self.rects.len() as i32);
         }
     }
 
